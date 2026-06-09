4b3d4902c47dbac77a11391a7c0f5a6a0dc7823d
PetBr1995 <dev.petersonbrito@gmail.com>
2026-06-09 16:54:19 -0400

    Resolução do exercicio 3

--- Lista_1/Exercicio_03/main.go
+++ Lista_1/Exercicio_03/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+/*Exercício 3. Considere o slice []int{10, 20, 30, 40, 50, 60, 70, 80, 90}. Inverta a ordem dos elementos sem usar um slice auxiliar e imprima o resultado.*/
+
 func main() {
-	fmt.Println("Teste")
+	list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	fmt.Println(list)
+	for i := 0; i < len(list)/2; i++ {
+		j := len(list) - 1 - i
+
+		list[i], list[j] = list[j], list[i]
+	}
+	fmt.Println(list)
+
 }
